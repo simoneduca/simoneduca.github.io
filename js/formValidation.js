629c2a22c4af03fa4381eed232ea95a8ae49ef01
Simone Duca <simone@zooniverse.org>
2016-01-29 15:04:25 +0000

    import site

---
+++ js/formValidation.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    
+    $('#contactForm').validate({
+        rules: {
+            name: {
+                minlength: 4,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                minlength: 4,
+                required: true
+            },
+            message: {
+                minlength: 4,
+                required: true
+            }
+        },
+        highlight: function (element) {
+            $(element).closest('.control-group').removeClass('success').addClass('error');
+            
+        },
+        success: function (element) {
+            element.text('OK!').addClass('valid')
+                .closest('.control-group').removeClass('error').addClass('success');   
+        }
+    });
+});
