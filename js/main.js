466038b5369b4db5f50b921e913584275bf86f37
Simone Duca <simone@zooniverse.org>
2017-10-25 12:51:47 +0100

    Fix navbar scrolling to anchors

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
     $(window).scroll(function () {
         if ($('.navbar.navbar-fixed-top').offset().top > 300) {
-           
+
             $('.navbar-header').removeClass('hidden');
         } else {
-           
+
             $('.navbar-header').addClass('hidden');
         }
     });
@@ -21,19 +21,19 @@
 			$("html, body").animate({ scrollTop: 0 }, 1000);
 				return false;
 		});
-	
+
 	// local scroll
 		jQuery('.navbar').localScroll({ hash: true, offset: { top: 0 }, duration: 800, easing: 'easeInOutExpo' });
 		jQuery('.navbar').localScroll({ hash: true, offset: { top: 0 }, duration: 800, easing: 'easeInOutExpo' });
 
-	
+
 	// portfolio
 //    if($('.isotopeWrapper').length){
 
 //        var $container = $('.isotopeWrapper');
 //        var $resize = $('.isotopeWrapper').attr('id');
 //        // initialize isotope
-        
+
 //        $container.isotope({
 //            itemSelector: '.isotopeItem',
 //            resizable: false, // disable normal resizing
@@ -42,9 +42,9 @@
 //            }
 
 
-            
+
 //        });
-        
+
 //        $container.imagesLoaded(function () {
 
 //            $container.isotope({
@@ -66,8 +66,8 @@
 //            });
 //            return false;
 //        });
-        
-        
+
+
 //        $(window).smartresize(function(){
 //            $container.isotope({
 //                // update columnWidth to a percentage of container width
@@ -76,9 +76,9 @@
 //                }
 //            });
 //        });
-        
 
-//}  
+
+//}
 
 
 	// fancybox
@@ -87,7 +87,7 @@
 
 	if (Modernizr.mq("screen and (max-width:1024px)")) {
 			jQuery("body").toggleClass("body");
-			
+
 	} else {
 		var s = skrollr.init({
 			mobileDeceleration: 1,
@@ -101,7 +101,7 @@
 						return 1-p;
 					}
 				}
-			});	
+			});
 	}
 
 	//scroll menu
@@ -109,7 +109,7 @@
 	jQuery(".appear").on("appear", function(data) {
 			var id = $(this).attr("id");
 			jQuery('.nav li').removeClass('active');
-			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
 		});
 
 
@@ -120,7 +120,7 @@
             isMobile = true;
         }
 
-        
+
         if (isMobile == false && ($('#parallax1').length  ||isMobile == false &&  $('#parallax2').length ||isMobile == false &&  $('#testimonials').length))
         {
 
@@ -135,7 +135,41 @@
             });
 
         }
-	
+	/**
+ * Check a href for an anchor. If exists, and in document, scroll to it.
+ * If href argument ommited, assumes context (this) is HTML Element,
+ * which will be the case when invoked by jQuery after an event
+ */
+	function scroll_if_anchor(href) {
+		console.log('scroll_if_anchor(href)', href)
+		href = typeof(href) == "string" ? href : $(this).attr("href");
+
+		// You could easily calculate this dynamically if you prefer
+		var fromTop = 50;
+
+		// If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+		// Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+		if(href.indexOf("#") == 0) {
+				var $target = $(href);
+
+				// Older browser without pushState might flicker here, as they momentarily
+				// jump to the wrong position (IE < 10)
+				if($target.length) {
+						$('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+						if(history && "pushState" in history) {
+								history.pushState({}, document.title, window.location.pathname + href);
+								return false;
+						}
+				}
+		}
+	}
+
+	// When our page loads, check to see if it contains and anchor
+	scroll_if_anchor(window.location.hash);
+
+	// Intercept all anchor clicks
+	$("body").on("click", "a", scroll_if_anchor);
+
 	//nicescroll
 	$("html").niceScroll({zindex:999,cursorborder:"",cursorborderradius:"2px",cursorcolor:"#191919",cursoropacitymin:.5});
 	function initNice() {
